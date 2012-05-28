2d682550b2d728745c4f1998c05edeeafefdaa43
Ragnar Dahlen <r.dahlen@gmail.com>
2012-05-28 22:18:53 +0200

    Add simple demo application showing possible usage of MSBuild tasks.

---
+++ GoogleClosure.Demo/Scripts/demo/Controller.js
@@ -0,0 +1,41 @@
+﻿goog.provide('demo.Controller');
+
+goog.require('goog.events');
+goog.require('goog.net.Jsonp');
+
+goog.require('demo.View');
+
+/**
+* Simple Controller-ish class responsible for handling user interaction,
+* communicating with the rest of the world (ie. the Flickr API) and 
+* updating the view as required.
+*
+* @constructor
+*/
+demo.Controller = function() {
+  this.view = new demo.View(this);
+  this.jsonp = new goog.net.Jsonp(this.view.getSearchUrl(), 'jsoncallback');
+  this.performSearch();
+};
+
+demo.Controller.prototype.performSearch = function() {
+  
+  var tags = this.view.getTags();
+
+  var payload = {
+    'format': 'json',
+    'tags': tags.join(',')
+  };
+
+  this.view.showMessage('Searching public Flickr feed...');
+  this.jsonp.send(payload, goog.bind(this.onReplyReceived, this), goog.bind(this.onError, this));
+};
+
+demo.Controller.prototype.onReplyReceived = function(response) {
+  this.view.update(response);
+};
+
+demo.Controller.prototype.onError = function(payload) {
+  console.log('error', payload);
+};
+
