20e6ddb043b8ca57360e41f38d555443632df451
Ragnar Dahlen <r.dahlen@gmail.com>
2012-05-30 09:29:50 +0200

    Add demo app

---
+++ demo/Scripts/demo/View.js
@@ -0,0 +1,67 @@
+﻿goog.provide('demo.View');
+
+goog.require('goog.dom');
+goog.require('goog.dom.forms');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
+goog.require('goog.string');
+
+
+/**
+* @constructor
+*/
+demo.View = function (controller) {
+  this.controller = controller;
+  this.searchForm = goog.dom.getElement('searchForm');
+  this.tagsInput = goog.dom.getElement('tagsInput');
+  this.resultDiv = goog.dom.getElement('resultDiv');
+
+  this.registerEvents();
+};
+
+demo.View.prototype.registerEvents = function() {
+  goog.events.listen(this.searchForm, goog.events.EventType.SUBMIT, this.onSearchFormSubmit, false, this);
+};
+
+demo.View.prototype.onSearchFormSubmit = function(ev) {
+  ev.preventDefault();
+  this.controller.performSearch();
+};
+
+demo.View.prototype.getSearchUrl = function() {
+  return this.searchForm.getAttribute('action');
+};
+
+demo.View.prototype.getTags = function () {
+  var value = goog.dom.forms.getValue(this.tagsInput);
+
+  return value.split(/\s+/);
+};
+
+demo.View.prototype.showMessage = function(s) {
+  goog.dom.removeChildren(this.resultDiv);
+
+  goog.dom.appendChild(this.resultDiv, goog.dom.createDom('p', null, s));
+};
+
+demo.View.prototype.update = function (feed) {
+
+  var itemViews = goog.array.map(feed['items'], this.createItemView);
+
+  goog.dom.removeChildren(this.resultDiv);
+
+  goog.array.forEach(itemViews, function (view) {
+    goog.dom.appendChild(this.resultDiv, view);
+  }, this);
+
+};
+
+demo.View.prototype.createItemView = function (item) {
+  return goog.dom.createDom('div', { 'class': 'item' },
+    goog.dom.createDom('a', { 'href': item['link']},
+      goog.dom.createDom('h3', { 'class': 'itemTitle' }, item['title'])),
+    goog.dom.createDom('p', { 'class': 'itemAuthor' }, item['author']),
+    goog.dom.createDom('a', { 'href': item['link']},
+      goog.dom.createDom('img', { 'class': 'itemImage', 'src': item['media']['m'] })),
+    goog.dom.createDom('hr'));
+}
---
+++ demo/Scripts/demo/demo.js
@@ -0,0 +1,18 @@
+﻿goog.provide('demo');
+
+goog.require('demo.Controller');
+
+/*
+* This function bootstraps the demo application by creating a new 
+* instance of the demo.Controller class.
+*/
+demo.initialize = function() {
+  window.controller = new demo.Controller();
+};
+
+/*
+* We want to make sure demo.initialize is callable from the HTML page 
+* after compilation and potential renaming. This can be accomplished
+* by the goog.exportSymbol function.
+*/
+goog.exportSymbol('demo.initialize', demo.initialize);
